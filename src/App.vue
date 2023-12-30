4a56e192f775324f1f5d559ddeda5e19e3e74e62
Niklas Fjeldberg <github@fjeldberg.me>
2023-12-30 11:58:50 +0100

    fix: wrong content type

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="mx-auto max-w-[800px] border-2">
-    <StrapiBlocks :content="data" />
+    <StrapiBlocks :content="data as BlocksContent" />
   </div>
 </template>
 
 <script setup lang="ts">
 import data from './data/data.json';
 import { useStrapiBlocks as StrapiBlocks } from './composables';
+import { type BlocksContent } from './types';
 </script>
